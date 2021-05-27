c0c6616858b85257011a59410782be9dd8cf6328
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-05-27 17:37:28 +0530

    feat: Simple Vote increment logic

--- bhumi_awards_portal/bhumi_awards_portal/doctype/vote/vote.py
+++ bhumi_awards_portal/bhumi_awards_portal/doctype/vote/vote.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2021, Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Vote(Document):
-	pass
+	def after_insert(self):
+		# Increase the vote count for this particular college
+		self.increment_vote_count()
+
+	def increment_vote_count(self):
+		college = frappe.get_doc('College', self.college)
+		college.total_votes = college.total_votes + 1
+		college.save()
