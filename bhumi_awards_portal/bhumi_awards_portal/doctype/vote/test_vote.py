e50bb364c12a238354ac94250fab05bc04dc4957
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-05-31 00:41:00 -0700

    fix: College name

--- bhumi_awards_portal/bhumi_awards_portal/doctype/vote/test_vote.py
+++ bhumi_awards_portal/bhumi_awards_portal/doctype/vote/test_vote.py
@@ -9,7 +9,7 @@ class TestVote(unittest.TestCase):
 		# Create a test college
 		test_college = frappe.get_doc({
 			'doctype': 'College',
-			'college_name': 'test college',
+			'college_name': 'test college 0',
 			'location': 'test location'
 		}).insert(ignore_permissions=True)
 
@@ -17,7 +17,7 @@ class TestVote(unittest.TestCase):
 		self.assertEqual(test_college.total_votes, 0)
 
 		# Cast a vote
-		frappe.get_doc({
+		test_vote = frappe.get_doc({
 			'doctype': 'Vote',
 			'college': test_college.name,
 			'voter_contact': '8776554534'
@@ -28,3 +28,6 @@ class TestVote(unittest.TestCase):
 
 		# Vote count should increment
 		self.assertEqual(test_college.total_votes, 1)
+
+		test_vote.delete()
+		test_college.delete( )
