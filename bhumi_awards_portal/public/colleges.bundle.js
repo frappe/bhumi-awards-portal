2bafca092915dab041e407294dd338771ab03083
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-06-01 08:11:59 -0700

    feat: Integrate frappe chart

---
+++ bhumi_awards_portal/public/colleges.bundle.js
@@ -0,0 +1,1 @@
+console.log('college js loaded');
--- bhumi_awards_portal/public/leaderboard.bundle.js
+++ bhumi_awards_portal/public/leaderboard.bundle.js
@@ -1,1 +1,14 @@
-import 'frappe-charts';
+import { Chart } from 'frappe-charts';
+
+new Chart("#leaderboard-chart", {
+    data: {
+        labels: ["IIT Bombay", "IIT Kanpur", "NIT Raipur"],
+        datasets: [
+            {
+                "name": "College",
+                "values": [10, 20, 30]
+            }
+        ]
+    },
+    type: "bar"
+});
