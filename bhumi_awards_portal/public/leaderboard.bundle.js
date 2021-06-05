eaea69047ae48f479550d93e93707be4c29cb410
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-06-05 16:50:59 +0530

    feat: College number displays in Leaderboard

--- bhumi_awards_portal/public/leaderboard.bundle.js
+++ bhumi_awards_portal/public/leaderboard.bundle.js
@@ -16,11 +16,50 @@ new Chart("#leaderboard-chart", {
 
 // DOM elements
 const rankTableBody = document.getElementById('rank-table-body');
+const pageLimitButtons = document.getElementsByClassName('page-limit-button');
+
+for (let b of pageLimitButtons) {
+    b.addEventListener('click', (e) => {
+        const numCollegesToDisplay = e.target.innerHTML;
+
+        if (numCollegesToDisplay == 'ALL') {
+            refreshTable();
+        } else {
+            refreshTable(parseInt(numCollegesToDisplay));
+        }
+
+        // Remove 'active' class from all buttons
+        Array.from(pageLimitButtons).forEach(b => b.parentElement.classList.remove('active'));
+
+        // Add to the clicked button
+        e.target.parentElement.classList.add('active');
+    });
+}
+
+function refreshTable(nOfColleges=null, nameQuery='') {
+    const args = {};
+
+    if (nOfColleges) {
+        args.limit = nOfColleges;
+    }
+    
+    if (nameQuery) {
+        args.name_query = nameQuery;
+    }
+
+    frappe.call({
+        method: 'bhumi_awards_portal.api.get_colleges_by_rank',
+        args: args,
+        callback: (data) => {
+          populateRankTable(data.message);
+        }
+    });
+}
 
 function populateRankTable(collegeData) {
     let html = '';
 
-    for (college of collegeData) {
+    for (let college of collegeData) {
         let rowHTML = `<tr>
             <th scope="row">${college.rank}</th>
             <td>${college.name}</td>
@@ -34,9 +73,4 @@ function populateRankTable(collegeData) {
     rankTableBody.innerHTML = html;
 }
 
-frappe.call({
-    method: 'bhumi_awards_portal.api.get_colleges_by_rank',
-    callback: (data) => {
-      populateRankTable(data.message);
-    }
-});
+refreshTable(10, '');
