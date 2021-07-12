737ed038e8520f8d112db6c945cab3edacb28ef9
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-07-12 23:09:57 +0530

    feat: College Registration and Approval
    - add approval status and flag for college registered from web form
    
    - add college registration web form
    
    - show only approved colleges in listing and link field
    
    - add college registration to navbar and CTA

--- bhumi_awards_portal/bhumi_awards_portal/doctype/college/college.py
+++ bhumi_awards_portal/bhumi_awards_portal/doctype/college/college.py
@@ -5,10 +5,15 @@ import frappe
 from frappe.model.document import Document
 
 class College(Document):
+	def validate(self):
+		if not self.created_via_web_form:
+			self.approval_status = 'Approved'
+
 	@staticmethod
 	def get_with_rank(limit=None, name_query=None):
 		colleges = frappe.get_all(
-			'College', 
+			'College',
+			filters={'approval_status': 'Approved'},
 			fields=['name', 'total_votes', 'location'],
 			order_by='total_votes desc'
 		)
@@ -16,7 +21,7 @@ class College(Document):
 		for index, college in enumerate(colleges):
 			rank = index + 1
 			college.rank = rank
-		
+
 		# Filter based on college name
 		if name_query:
 			colleges = list(filter(lambda x: x.name.lower().startswith(name_query), colleges))
@@ -32,22 +37,22 @@ class College(Document):
 	def get_paginated(limit_start=0, limit_page_length=30, order_by=None, location_query=None, name_query=None):
 		if order_by is None:
 			order_by = 'total_votes desc'
-		
-		filters = {}
+
+		filters = {'approval_status': 'Approved'}
 
 		if name_query:
 			filters['name'] = ('like', f'%{name_query}%')
-		
+
 		if location_query:
 			filters['location'] = ('like', f'%{location_query}%')
 
 		colleges = frappe.get_all(
-			'College', 
+			'College',
 			fields=['name', 'total_votes', 'location'],
 			filters=filters,
 			order_by=order_by,
 			limit_start=limit_start,
 			limit_page_length=limit_page_length
 		)
-		
+
 		return colleges
